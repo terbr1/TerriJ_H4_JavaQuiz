e6f0f2204beb3ab71f6dca9882ef6e210777be98
Terri J <terbr@hotmail.com>
2020-05-29 22:31:47 -0700

    added timer and styling

--- quiz.js
+++ quiz.js
@@ -1,167 +1,98 @@
 var questionEl = document.getElementById("question");
-var choicesEl = document.getElementsByClassName("choice-text");
+var choicesEl = Array.from(document.getElementsByClassName("choice-text"));
+var timerEl = document.getElementById(timer);
+
 
 var currentQuestion = {};
 var acceptingAnswers = true;
-var score = 0;
-var questionCounter = 0;
 var availableQuestions = [];
 
- 
+//Questions
 var questions = [
-    {
-        question: "What is question 1?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 2
-
-    },
-    {
-        question: "What is question 2?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 1
-
-    },
-    {
-        question: "What is question 3?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 3
-
-    },
-    {
-        question: "What is question 4?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 1
-
-    },
-    {
-        question: "What is question 5?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 4
-
-    },
-    {
-        question: "What is question 6?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 2
-
-    },
-    {
-        question: "What is question 7?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 3
-
-    },
-    {
-        question: "What is question 8?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 4
-
-    },
-    {
-        question: "What is question 9?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 1
-
-    },
-    {
-        question: "What is question 10?",
-        choice1: "A",
-        choice2: "B",
-        choice3: "C",
-        choice4: "D",
-        answer: 2
-
-    },
+  {
+    question: "What is question 1?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    answer: 2,
+  },
+  {
+    question: "What is question 2?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    answer: 1,
+  },
+  {
+    question: "What is question 3?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    answer: 3,
+  },
+  {
+    question: "What is question 4?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    answer: 1,
+  },
+  {
+    question: "What is question 5?",
+    choice1: "A",
+    choice2: "B",
+    choice3: "C",
+    choice4: "D",
+    answer: 4,
+  },
 ];
 
-var maxQuestions = 10;
+var startTime = 2
+var totalTime = starttime * 60
 
-// Start Game 
-function startGame(){
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
-    getNewQuestion();
+// Start Game
+function startGame() {
+  availableQuestions = [...questions];
+  getNewQuestion();
 }
 
-
+}
 function getNewQuestion() {
-    questionCounter++;
-    var questionIndex = Math.floor(Math.random()* availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  if (availableQuestions.length === 0) {
+    return window.location.assign("index.html");
+  }
+  questionCounter++;
 
-};
-startGame();
+  var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
 
+  choicesEl.forEach((choice) => {
+    var number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+}
 
+choicesEl.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+    acceptingAnswers = false;
+    var selectedChoice = e.target;
+    var selectedAnswer = selectedChoice.dataset["number"];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function startgame() {
-    availableQuestions = [...questions];
-    console.log(availableQuestions);
     getNewQuestion();
-};
+  });
+});
 
-function getNewQuestion() {
-    var questioninterval = setInterval(() => {
-        questionEl.textContent = availableQuestions[questions];
-        currentQuestion++;
-        question.innerText = currentQuestion.question;
+function setTime(){
+    totalTime--;
+    
 
-        if (currentQuestion===questions.length){
-              console.log("it worked"); 
-
-
-    }
-})
-};
-
-  /*  //questionEl = availableQuestions[questions];
-    //currentQuestion++; //increment to one
-    //question.innerText = currentQuestion.question;
-    //if (currentQuestion===questions.length){
-      //  console.log("it worked") */
+startGame();
