819393d44a04436c527b0be26131650ea59b6e6f
Terri J <terbr@hotmail.com>
2020-05-30 08:00:04 -0700

    added high score html and js

--- quiz.js
+++ quiz.js
@@ -1,13 +1,14 @@
 var questionEl = document.getElementById("question");
 var choicesEl = Array.from(document.getElementsByClassName("choice-text"));
-var timerEl = document.getElementById(timer);
-
+var timerEl = document.getElementById("timer");
+//console.log(choices);
 
 var currentQuestion = {};
 var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
 var availableQuestions = [];
 
-//Questions
 var questions = [
   {
     question: "What is question 1?",
@@ -49,24 +50,48 @@ var questions = [
     choice4: "D",
     answer: 4,
   },
-];
+  ];
 
-var startTime = 2
-var totalTime = starttime * 60
+var startingTime = 2
+var totalTime = startingTime *60
+
+/*function setTimer() {
+    // Create the countdown timer.
+   var timerInterval = setInterval
+   (function() {
+     totalTime--;
+     timeEl.textContent = secondsLeft;
+     
+     if(secondsLeft === 0){
+       return window.location.assign("index.html")
+    getNewQuestion();  
+      }
+     
+   }, 1000);
+  }*/
 
 // Start Game
 function startGame() {
   availableQuestions = [...questions];
+  console.log(availableQuestions);
   getNewQuestion();
 }
 
-}
 function getNewQuestion() {
   if (availableQuestions.length === 0) {
     return window.location.assign("index.html");
   }
   questionCounter++;
-
+  var timerInterval = setInterval
+   (function() {
+     totalTime--;
+     timeEl.textContent = totalTime;
+     
+     if(totalTime === 0){
+       return window.location.assign("index.html") 
+      }
+     
+   }, 1000);
   var questionIndex = Math.floor(Math.random() * availableQuestions.length);
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -86,13 +111,10 @@ choicesEl.forEach((choice) => {
     acceptingAnswers = false;
     var selectedChoice = e.target;
     var selectedAnswer = selectedChoice.dataset["number"];
-
+    
     getNewQuestion();
   });
 });
 
-function setTime(){
-    totalTime--;
-    
 
 startGame();
